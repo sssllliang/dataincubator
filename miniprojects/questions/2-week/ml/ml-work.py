47f6d3325f00e7a19a805faefc9219c7812b9f27
r-handsfield <r.handsfield@gmail.com>
2015-06-08 11:06:48 -0400

    initial commit/ adds working file for w2p1

---
+++ miniprojects/questions/2-week/ml/ml-work.py
@@ -0,0 +1,34 @@
+# ml-work.py
+
+# #!/bin/bash
+# 
+# cd ~/miniprojects/questions/2-week/ml/data
+# wget http://thedataincubator.s3.amazonaws.com/coursedata/mldata/yelp_train_academic_dataset_business.json.gz
+#
+# gunzip yelp_train_academic_dataset_business.json.gz
+# 
+
+# json row looks like
+#==============================================================================
+# {
+# 	"business_id": "vcNAWiLM4dR7D2nwwJ7nCA", 
+# 	"full_address": "4840 E Indian School Rd\nSte 101\nPhoenix, AZ 85018", 
+# 	"hours": {"Tuesday": {"close": "17:00", "open": "08:00"}, 
+# 	          "Friday": {"close": "17:00", "open": "08:00"}, 
+# 		    "Monday": {"close": "17:00", "open": "08:00"}, 
+# 		    "Wednesday": {"close": "17:00", "open": "08:00"}, 
+# 		    "Thursday": {"close": "17:00", "open": "08:00"}
+# 		   }, 
+# 	"open": true, 
+# 	"categories": ["Doctors", "Health & Medical"], 
+# 	"city": "Phoenix", "review_count": 7, 
+# 	"name": "Eric Goldberg, MD", 
+# 	"neighborhoods": [], 
+# 	"longitude": -111.98375799999999, 
+# 	"state": "AZ", "stars": 3.5, 
+# 	"latitude": 33.499313000000001, 
+# 	"attributes": {"By Appointment Only": true}, 
+#	"type": "business"
+# }
+#==============================================================================
+
